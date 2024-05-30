9e61e9b9168f1d50ca76e508afc6b9e6864a36dc
ipsso <ipsso@host.docker.internal>
2024-05-30 23:05:07 +0530

    Second Inheritance One

---
+++ Example/src/firstProject/InheritanceOne.java
@@ -0,0 +1,10 @@
+package firstProject;
+
+public class InheritanceOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Inheritance");
+	}
+
+}
