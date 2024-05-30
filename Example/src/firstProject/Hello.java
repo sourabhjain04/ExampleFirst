e3eae555e332631cfae6471bdee7da66260c6547
ipsso <ipsso@host.docker.internal>
2024-05-30 20:00:23 +0530

    first push

---
+++ Example/src/firstProject/Hello.java
@@ -0,0 +1,10 @@
+package firstProject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is java");
+	}
+
+}
