bf5293ac4595bb89ecdb626ae68bb2d3a124315b
ipsso <ipsso@host.docker.internal>
2024-06-07 16:16:48 +0530

    Actuator

---
+++ demoActuator/src/main/java/com/example/demo/All/WelcomeRestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.All;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return "Welcome to India";
+	}
+}
